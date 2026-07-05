4caeae1f3c929a2de533bab5ca653ecff6a15946
zlongacre <zachary.longacre@gmail.com>
2026-07-05 15:02:06 -0400

    Initial commit

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,27 @@
+'''Creating an emotion detection application using the Watson NLP library'''
+
+import requests
+
+def emotion_detector(text_to_analyze):
+    '''Uses url to input defined text to watson emotion detector 
+       which analyzes and outputs emotion with score'''
+
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    my_input = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    response = requests.post(url, json=my_input, headers=header, timeout=10)
+
+    if response.status_code != 200:
+        return None
+
+    formatted_response = response.json()
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+
+    return emotions
